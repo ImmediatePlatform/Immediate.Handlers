981c83f755f9486b42a8dd0b11d262f9592a3686
Stuart Turner <stuart@turner-isageek.com>
2024-01-06 14:52:15 -0600

    Remove unnecessary interface

--- src/Immediate.Handlers.Shared/IAssemblyMarker.cs
+++
@@ -1,8 +0,0 @@
-namespace Immediate.Handlers.Shared;
-
-/// <summary>
-/// A marker interface for the assembly.
-/// </summary>
-#pragma warning disable CA1040
-public interface IAssemblyMarker;
-#pragma warning restore CA1040
