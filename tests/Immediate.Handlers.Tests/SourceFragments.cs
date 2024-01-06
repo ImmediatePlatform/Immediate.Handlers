dc278d6d04dfe38341a7e1a87667310fd8e7f29e
Duke <commits@dmail.icu>
2024-01-06 16:15:11 +0100

    Fix indentation for normal source fragment

--- tests/Immediate.Handlers.Tests/SourceFragments.cs
+++ tests/Immediate.Handlers.Tests/SourceFragments.cs
@@ -3,56 +3,57 @@ namespace Immediate.Handlers.Tests;
 public static class SourceFragments
 {
 	public static readonly string Normal = """
-           using Dummy;
-           using Immediate.Handlers.Shared;
+using Dummy;
+using Immediate.Handlers.Shared;
 
-           [assembly: RenderMode(renderMode: RenderMode.Normal)]
+[assembly: RenderMode(renderMode: RenderMode.Normal)]
 
-           [assembly: Behaviors(
-            typeof(LoggingBehavior<,>)
-           )]
+[assembly: Behaviors(
+	typeof(LoggingBehavior<,>)
+)]
 
-           namespace Dummy;
+namespace Dummy;
 
-           public class GetUsersEndpoint(GetUserQuery.Handler handler)
-           {
-            public async Task<IEnumerable<User>> GetUsers() =>
-                handler.HandleAsync(new GetUsersQuery.Query());
-           }
+public class GetUsersEndpoint(GetUsersQuery.Handler handler)
+{
+	public async Task<IEnumerable<User>> GetUsers() =>
+		handler.HandleAsync(new GetUsersQuery.Query());
+}
 
-           [Handler]
-           public static class GetUserQuery
-           {
-            public record Query;
+[Handler]
+public static class GetUsersQuery
+{
+	public record Query;
+
+	private static Task<IEnumerable<User>> HandleAsync(
+		Query _,
+		UsersService usersService,
+		CancellationToken token)
+	{
+		return usersService.GetUsers();
+	}
+}
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+	: Behavior<TRequest, TResponse>
+{
+	public override async Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken)
+	{
+		var response = await InnerHandler.HandleAsync(request, cancellationToken);
+
+		return response;
+	}
+}
+
+public class User { }
+public class UsersService
+{
+	public Task<IEnumerable<User>> GetUsers() =>
+		Task.FromResult(Enumerable.Empty<User>());
+}
+
+public interface ILogger<T>;
+   
            
-            private static Task<IEnumerable<User>> HandleAsync(
-                Query _,
-                UsersService usersService,
-                CancellationToken token)
-            {
-                return usersService.GetUsers();
-            }
-           }
-
-           public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
-            : Behavior<TRequest, TResponse>
-           {
-            public override async Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken)
-            {
-                var response = await InnerHandler.HandleAsync(request, cancellationToken);
-           
-                return response;
-            }
-           }
-
-           public class User { }
-           public class UsersService
-           {
-            public Task<IEnumerable<User>> GetUsers() =>
-                Task.FromResult(Enumerable.Empty<User>());
-           }
-
-           public interface ILogger<T>;
-
-           """;
+""";
 }
