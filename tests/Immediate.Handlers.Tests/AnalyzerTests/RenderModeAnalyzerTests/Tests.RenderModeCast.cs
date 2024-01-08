c9d26edf21b1016fb0f15ac5241de5a00016c9f1
Stuart Turner <stuart@turner-isageek.com>
2024-01-08 11:11:03 -0600

    Add tests for `RenderModeAnalyzer`

---
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/RenderModeAnalyzerTests/Tests.RenderModeCast.cs
@@ -0,0 +1,52 @@
+using Immediate.Handlers.Analyzers;
+using Immediate.Handlers.Tests.Helpers;
+using Verifier =
+	Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+		Immediate.Handlers.Analyzers.RenderModeAnalyzer>;
+
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public partial class Tests
+{
+	[Fact]
+	public async Task RenderModeCast_Alerts()
+	{
+		const string Text = """
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Immediate.Handlers.Shared;
+
+[assembly: RenderMode((RenderMode)1)]
+
+[Handler]
+[RenderMode((RenderMode)1)]
+public static class GetUsersQuery
+{
+	public record Query;
+
+	private static Task<int> HandleAsync(
+		Query _,
+		CancellationToken token)
+	{
+		return Task.FromResult(0);
+	}
+}
+""";
+
+		var test = AnalyzerTestHelpers.CreateAnalyzerTest<RenderModeAnalyzer>(
+			Text,
+			DriverReferenceAssemblies.Normal,
+			[
+				Verifier.Diagnostic("IHR0004").WithLocation(10, 12),
+				Verifier.Diagnostic("IHR0004").WithLocation(13, 2),
+			]
+		);
+		await test.RunAsync();
+	}
+}
---
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/RenderModeAnalyzerTests/Tests.RenderModeIsNormal.cs
@@ -0,0 +1,46 @@
+using Immediate.Handlers.Analyzers;
+using Immediate.Handlers.Tests.Helpers;
+
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public partial class Tests
+{
+	[Fact]
+	public async Task RenderModeIsNormal_DoesNotAlert()
+	{
+		const string Text = """
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Immediate.Handlers.Shared;
+
+[assembly: RenderMode(RenderMode.Normal)]
+
+[Handler]
+[RenderMode(RenderMode.Normal)]
+public static class GetUsersQuery
+{
+	public record Query;
+
+	private static Task<int> HandleAsync(
+		Query _,
+		CancellationToken token)
+	{
+		return Task.FromResult(0);
+	}
+}
+""";
+
+		var test = AnalyzerTestHelpers.CreateAnalyzerTest<RenderModeAnalyzer>(
+			Text,
+			DriverReferenceAssemblies.Normal,
+			[]
+		);
+		await test.RunAsync();
+	}
+}
