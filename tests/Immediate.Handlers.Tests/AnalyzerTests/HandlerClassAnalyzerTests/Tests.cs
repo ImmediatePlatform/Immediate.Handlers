a001f903c3fba99ee7c7a79c366304b1d4078d78
Stuart Turner <stuart@turner-isageek.com>
2024-01-08 06:59:16 -0600

    More reorganization

--- tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.cs
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.cs
@@ -4,9 +4,10 @@ using Verifier =
 	Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
 		Immediate.Handlers.Analyzers.HandlerClassAnalyzer>;
 
-namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzer;
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
 
-public class HandlerClassAnalyzerTests
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public class Tests
 {
 	[Fact]
 	public async Task HandleMethodDoesNotExist_AlertDiagnostic()
