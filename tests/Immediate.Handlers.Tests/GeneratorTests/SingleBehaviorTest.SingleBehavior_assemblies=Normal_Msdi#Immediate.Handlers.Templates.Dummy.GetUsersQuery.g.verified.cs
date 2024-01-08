0a748184f30f8790f84a869734b330abc2835fcb
Stuart Turner <stuart@turner-isageek.com>
2024-01-07 21:10:22 -0600

    Improve rendered code layout

--- tests/Immediate.Handlers.Tests/GeneratorTests/SingleBehaviorTest.SingleBehavior_assemblies=Normal_Msdi#Immediate.Handlers.Templates.Dummy.GetUsersQuery.g.verified.cs
+++ tests/Immediate.Handlers.Tests/GeneratorTests/SingleBehaviorTest.SingleBehavior_assemblies=Normal_Msdi#Immediate.Handlers.Templates.Dummy.GetUsersQuery.g.verified.cs
@@ -9,21 +9,25 @@ public static partial class GetUsersQuery
 {
     public sealed class Handler
     {
-        private readonly global::Dummy.LoggingBehavior<global::Dummy.GetUsersQuery.Query, IEnumerable<global::Dummy.User>> _behavior_1;
         private readonly global::Dummy.GetUsersQuery.HandleBehavior _behavior_0;
+        private readonly global::Dummy.LoggingBehavior<global::Dummy.GetUsersQuery.Query, IEnumerable<global::Dummy.User>> _behavior_1;
     
         public Handler(
-            global::Dummy.LoggingBehavior<global::Dummy.GetUsersQuery.Query, IEnumerable<global::Dummy.User>> behavior_1,
-            global::Dummy.GetUsersQuery.HandleBehavior handleBehavior)
+            global::Dummy.GetUsersQuery.HandleBehavior behavior_0,
+            global::Dummy.LoggingBehavior<global::Dummy.GetUsersQuery.Query, IEnumerable<global::Dummy.User>> behavior_1
+		)
         {
+            _behavior_0 = behavior_0;
             _behavior_1 = behavior_1;
-            _behavior_0 = handleBehavior;
         }
     
         public async global::System.Threading.Tasks.Task<IEnumerable<global::Dummy.User>> HandleAsync(global::Dummy.GetUsersQuery.Query request, global::System.Threading.CancellationToken cancellationToken = default)
         {
             _behavior_1.InnerHandler = _behavior_0;
-            return await _behavior_1.HandleAsync(request, cancellationToken).ConfigureAwait(false);
+			
+            return await _behavior_1
+				.HandleAsync(request, cancellationToken)
+				.ConfigureAwait(false);
         }
     }
 
@@ -41,10 +45,13 @@ public static partial class GetUsersQuery
     
         public override async global::System.Threading.Tasks.Task<IEnumerable<global::Dummy.User>> HandleAsync(global::Dummy.GetUsersQuery.Query request, global::System.Threading.CancellationToken cancellationToken)
         {
-            return await global::Dummy.GetUsersQuery.HandleAsync(
-                request,                
-                _usersService,
-                cancellationToken).ConfigureAwait(false);
+            return await global::Dummy.GetUsersQuery
+				.HandleAsync(
+					request,                
+					_usersService,
+					cancellationToken
+				)
+				.ConfigureAwait(false);
         }
     }
 
