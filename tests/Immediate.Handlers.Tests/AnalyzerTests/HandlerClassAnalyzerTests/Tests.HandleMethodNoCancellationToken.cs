550eb28ac1d315a3e7a1c754ebc6da978d2b6059
Stuart Turner <stuart@turner-isageek.com>
2024-01-08 07:15:40 -0600

    Add more test coverage of analyzer

---
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.HandleMethodNoCancellationToken.cs
@@ -0,0 +1,50 @@
+using Immediate.Handlers.Analyzers;
+using Immediate.Handlers.Tests.Helpers;
+using Verifier =
+	Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+		Immediate.Handlers.Analyzers.HandlerClassAnalyzer>;
+
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public partial class Tests
+{
+	[Fact]
+	public async Task HandleMethodMissingCancellationToken_AlertDiagnostic()
+	{
+		const string Text = """
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Immediate.Handlers.Shared;
+
+[Handler]
+public static class GetUsersQuery
+{
+	public record Query;
+
+	private static Task<int> HandleAsync(
+		Query _,
+		int parameter2)
+	{
+		return Task.FromResult(0);
+	}
+}
+""";
+
+		var expected = Verifier.Diagnostic("IH0003")
+			.WithLocation(15, 27)
+			.WithArguments("HandleAsync");
+
+		var test = AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
+			Text,
+			DriverReferenceAssemblies.Normal,
+			[expected]
+		);
+		await test.RunAsync();
+	}
+}
