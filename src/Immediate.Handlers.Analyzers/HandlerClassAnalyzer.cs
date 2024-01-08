bc342299e4aed5bc608e23da06f9a3905b91a1b5
Duke <commits@dmail.icu>
2024-01-08 14:31:48 +0100

    Add support for Task aswell as Task<T> as a handleasync method return type

--- src/Immediate.Handlers.Analyzers/HandlerClassAnalyzer.cs
+++ src/Immediate.Handlers.Analyzers/HandlerClassAnalyzer.cs
@@ -21,12 +21,12 @@ public class HandlerClassAnalyzer : DiagnosticAnalyzer
 	private static readonly DiagnosticDescriptor HandlerMethodMustReturnTask =
 		new(
 			id: DiagnosticIds.IH0002HandlerMethodMustReturnTask,
-			title: "Handler method must return a Task<T>",
-			messageFormat: "Method '{0}' must return a Task<T>",
+			title: "Handler method must return a Task",
+			messageFormat: "Method '{0}' must return a Task",
 			category: "ImmediateHandler",
 			defaultSeverity: DiagnosticSeverity.Error,
 			isEnabledByDefault: true,
-			description: "Handler methods must return a Task<T>."
+			description: "Handler methods must return a Task."
 		);
 
 	private static readonly DiagnosticDescriptor HandlerMethodMustReceiveCorrectParameters =
@@ -89,7 +89,7 @@ public class HandlerClassAnalyzer : DiagnosticAnalyzer
 		}
 
 		if (methodSymbol.ReturnType is INamedTypeSymbol returnTypeSymbol
-			&& returnTypeSymbol.ConstructedFrom.ToString() is not "System.Threading.Tasks.Task<TResult>")
+			&& returnTypeSymbol.ConstructedFrom.ToString() is not ("System.Threading.Tasks.Task<TResult>" or "System.Threading.Tasks.Task"))
 		{
 			var mustReturnTaskT = Diagnostic.Create(
 				HandlerMethodMustReturnTask,
