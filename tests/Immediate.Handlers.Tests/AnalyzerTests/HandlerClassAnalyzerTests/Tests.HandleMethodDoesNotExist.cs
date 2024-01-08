56ffa244420f154bf33bf76f5a77c1e8984cd653
Stuart Turner <stuart@turner-isageek.com>
2024-01-08 07:04:32 -0600

    more test reorganization

---
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.HandleMethodDoesNotExist.cs
@@ -0,0 +1,43 @@
+using Immediate.Handlers.Analyzers;
+using Immediate.Handlers.Tests.Helpers;
+using Verifier =
+	Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+		Immediate.Handlers.Analyzers.HandlerClassAnalyzer>;
+
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public partial class Tests
+{
+	[Fact]
+	public async Task HandleMethodDoesNotExist_AlertDiagnostic()
+	{
+		const string Text = """
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Immediate.Handlers.Shared;
+
+[Handler]
+public static class GetUsersQuery
+{
+	public record Query;
+}
+""";
+
+		var expected = Verifier.Diagnostic("IH0001")
+			.WithLocation(11, 21)
+			.WithArguments("GetUsersQuery");
+
+		var test = AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
+			Text,
+			DriverReferenceAssemblies.Normal,
+			[expected]
+		);
+		await test.RunAsync();
+	}
+}
--- tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.HandleMethodDoesNotReturnTask.cs
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.HandleMethodDoesNotReturnTask.cs
@@ -7,40 +7,8 @@ using Verifier =
 namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
-public class Tests
+public partial class Tests
 {
-	[Fact]
-	public async Task HandleMethodDoesNotExist_AlertDiagnostic()
-	{
-		const string Text = """
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Immediate.Handlers.Shared;
-
-[Handler]
-public static class GetUsersQuery
-{
-	public record Query;
-}
-""";
-
-		var expected = Verifier.Diagnostic("IH0001")
-			.WithLocation(11, 21)
-			.WithArguments("GetUsersQuery");
-
-		var test = AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
-			Text,
-			DriverReferenceAssemblies.Normal,
-			[expected]
-		);
-		await test.RunAsync();
-	}
-
 	[Fact]
 	public async Task HandleMethodDoesNotReturnTask_AlertDiagnostic()
 	{
@@ -83,44 +51,6 @@ public interface ILogger<T>;
 
 		var expected = Verifier.Diagnostic("IH0002")
 			.WithLocation(15, 36)
-			.WithArguments("HandleAsync");
-
-		var test = AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
-			Text,
-			DriverReferenceAssemblies.Normal,
-			[expected]
-		);
-		await test.RunAsync();
-	}
-
-	[Fact]
-	public async Task HandleMethodDoesNotDefineTwoParameters_AlertDiagnostic()
-	{
-		const string Text = """
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Immediate.Handlers.Shared;
-
-[Handler]
-public static class GetUsersQuery
-{
-	public record Query;
-
-	private static Task<int> HandleAsync(
-		Query _)
-	{
-		return Task.FromResult(0);
-	}
-}
-""";
-
-		var expected = Verifier.Diagnostic("IH0003")
-			.WithLocation(15, 27)
 			.WithArguments("HandleAsync");
 
 		var test = AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
