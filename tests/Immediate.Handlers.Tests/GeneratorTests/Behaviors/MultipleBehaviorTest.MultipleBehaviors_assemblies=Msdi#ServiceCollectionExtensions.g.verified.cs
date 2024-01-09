397f98cce91d9b665eef5c0b28d9ef4d25c3b5ec
Stuart Turner <stuart@turner-isageek.com>
2024-01-09 14:02:18 -0600

    Separate `Handler` and `Behavior` registration

--- tests/Immediate.Handlers.Tests/GeneratorTests/Behaviors/MultipleBehaviorTest.MultipleBehaviors_assemblies=Msdi#ServiceCollectionExtensions.g.verified.cs
+++ tests/Immediate.Handlers.Tests/GeneratorTests/Behaviors/MultipleBehaviorTest.MultipleBehaviors_assemblies=Msdi#ServiceCollectionExtensions.g.verified.cs
@@ -5,11 +5,18 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class HandlerServiceCollectionExtensions
 {
-	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddHandlers(
+	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddBehaviors(
 		this global::Microsoft.Extensions.DependencyInjection.IServiceCollection services)
 	{
 		services.AddScoped(typeof(global::Dummy.LoggingBehavior<,>));
 		services.AddScoped(typeof(global::Dummy.SecondLoggingBehavior<,>));
+		
+		return services;
+	}
+
+	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddHandlers(
+		this global::Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+	{
 		global::Dummy.GetUsersQuery.AddHandlers(services);
 		
 		return services;
--- tests/Immediate.Handlers.Tests/GeneratorTests/Behaviors/SingleBehaviorTest.SingleBehavior_assemblies=Msdi#ServiceCollectionExtensions.g.verified.cs
+++ tests/Immediate.Handlers.Tests/GeneratorTests/Behaviors/SingleBehaviorTest.SingleBehavior_assemblies=Msdi#ServiceCollectionExtensions.g.verified.cs
@@ -5,10 +5,17 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class HandlerServiceCollectionExtensions
 {
-	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddHandlers(
+	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddBehaviors(
 		this global::Microsoft.Extensions.DependencyInjection.IServiceCollection services)
 	{
 		services.AddScoped(typeof(global::Dummy.LoggingBehavior<,>));
+		
+		return services;
+	}
+
+	public static global::Microsoft.Extensions.DependencyInjection.IServiceCollection AddHandlers(
+		this global::Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+	{
 		global::Dummy.GetUsersQuery.AddHandlers(services);
 		
 		return services;
