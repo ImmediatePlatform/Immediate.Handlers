07b24063be4db516c62786da10b61748e2a1316a
Stuart Turner <stuart@turner-isageek.com>
2024-01-09 09:27:08 -0600

    fix namespace

--- tests/Immediate.Handlers.Tests/AnalyzerTests/RenderModeAnalyzerTests/Tests.RenderModeIsNormal.cs
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/RenderModeAnalyzerTests/Tests.RenderModeIsNormal.cs
@@ -1,7 +1,7 @@
 using Immediate.Handlers.Analyzers;
 using Immediate.Handlers.Tests.Helpers;
 
-namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+namespace Immediate.Handlers.Tests.AnalyzerTests.RenderModeAnalyzerTests;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
 public partial class Tests
