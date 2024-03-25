693bef8a67504ab871eac6c57d5e7208cf590bbc
Stuart Turner <stuart@turner-isageek.com>
2024-03-25 07:17:28 -0500

    Add diagnostic if `Handle` method is not `private`

---
+++ tests/Immediate.Handlers.Tests/AnalyzerTests/HandlerClassAnalyzerTests/Tests.HandleMethodIsNotPrivate.cs
@@ -0,0 +1,37 @@
+using Immediate.Handlers.Analyzers;
+using Immediate.Handlers.Tests.Helpers;
+
+namespace Immediate.Handlers.Tests.AnalyzerTests.HandlerClassAnalyzerTests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Not being consumed by other code")]
+public partial class Tests
+{
+	[Fact]
+	public async Task HandleMethodIsNotPrivate_AlertDiagnostic() =>
+		await AnalyzerTestHelpers.CreateAnalyzerTest<HandlerClassAnalyzer>(
+			"""
+			using System;
+			using System.Collections.Generic;
+			using System.IO;
+			using System.Linq;
+			using System.Net.Http;
+			using System.Threading;
+			using System.Threading.Tasks;
+			using Immediate.Handlers.Shared;
+
+			[Handler]
+			public class GetUsersQuery
+			{
+				public record Query;
+			
+				public static ValueTask<int> {|IHR0011:HandleAsync|}(
+					Query _,
+					CancellationToken token)
+				{
+					return ValueTask.FromResult(0);
+				}
+			}
+			""",
+			DriverReferenceAssemblies.Normal
+		).RunAsync();
+}
