9678aa2296e182c5d631f06d0df4f26e22616f52
Stuart Turner <stuart@turner-isageek.com>
2026-01-02 20:59:05 -0600

    General updates

--- benchmarks/Benchmark.Large/Program.cs
+++ benchmarks/Benchmark.Large/Program.cs
@@ -1,4 +1,8 @@
 using BenchmarkDotNet.Running;
 using Immediate.Handlers.Benchmarks;
+using RhoMicro.BdnLogging;
 
-BenchmarkRunner.Run<RequestBenchmarks>();
+BenchmarkRunner.Run<RequestBenchmarks>(
+	SpotlightConfig.Instance,
+	args
+);
