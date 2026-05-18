9f4e6e8325d2b7ef28427d52a367400988579914
Stuart Turner <stuart@turner-isageek.com>
2026-05-18 13:10:40 -0500

    General Code Improvements

--- tests/Immediate.Handlers.Tests/ModuleInitializer.cs
+++ tests/Immediate.Handlers.Tests/ModuleInitializer.cs
@@ -8,6 +8,9 @@ public static class ModuleInitializer
 	public static void Init()
 	{
 		VerifierSettings.AutoVerify(includeBuildServer: false);
+		VerifierSettings.ScrubLinesContaining("cs", comparison: StringComparison.Ordinal, "GeneratedCodeAttribute");
+		UseSourceFileRelativeDirectory("Snapshots");
+
 		VerifySourceGenerators.Initialize();
 	}
 }
